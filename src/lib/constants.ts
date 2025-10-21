4ddf7242533387325323dbac8fcc212951a993db
Dan <danil.mlnchk@gmail.com>
2025-10-21 09:00:50 +0700

    Remove trailing slash from production APP_URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,5 @@ export const MY_ID = 43044295; // real
 
 // TODO: move to env
 export const APP_URL = import.meta.env.PROD
-  ? "https://billy.billy-bot.workers.dev"
+  ? "https://billy.greedyeugene.workers.dev"
   : "https://billy-dev-bot.loca.lt";
